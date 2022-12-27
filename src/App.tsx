b820200fd55b0b373f111b421722a3790d0a920e
ArtyomKub <kub.killjoy@gmail.com>
2022-12-27 14:35:01 +0300

    Add a new function Rating

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            This is APP component
+            <Rating/>
+        </div>
+    );
 }
+
+function Rating() {
+    return (
+        <div>
+            <div>Stars</div>
+            <div>Stars</div>
+            <div>Stars</div>
+            <div>Stars</div>
+            <div>Stars</div>
+        </div>
+    )
+}
+
 
 export default App;
