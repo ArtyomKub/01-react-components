7c0b90ef1988aaefbfaae148c75a760d38a1f25d
ArtyomKub <kub.killjoy@gmail.com>
2023-01-18 15:23:26 +0300

    Refactoring code in 'UncontrolledRating.tsx'

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,20 +1,18 @@
 import React, {useState} from "react";
 
-type RatingPropsType = {
+type RatingPropsType = {}
 
-}
+export function UncontrolledRating(props: RatingPropsType) {
 
-export function UncontrolledRating(props:RatingPropsType) {
-
-    let [value,setValue] = useState(4);
+    let [value, setValue] = useState(4);
 
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={()=>{setValue(1)}}>1</button>
-            <Star selected={value > 1}/><button onClick={()=>{setValue(2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={()=>{setValue(3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={()=>{setValue(4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={()=>{setValue(5)}}>5</button>
+            <Star selected={value > 0}/>
+            <Star selected={value > 1}/>
+            <Star selected={value > 2}/>
+            <Star selected={value > 3}/>
+            <Star selected={value > 4}/>
         </div>
     )
 }
@@ -24,13 +22,9 @@ type StarPropsType = {
 }
 
 function Star(props: StarPropsType) {
-    if (props.selected) {
-        return (
-            <span><b>Star </b></span>
-        )
-    } else {
-        return (
-            <span>Star </span>
-        )
-    }
+    return (
+        <span>
+            {props.selected ? <b>star </b> : 'star'}
+        </span>
+    )
 }
